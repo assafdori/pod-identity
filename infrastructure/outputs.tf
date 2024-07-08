938f7bf1cb2ad5538556b5bd01d94033d60ccb7b
Assaf Dori <assafxdori@gmail.com>
2024-07-08 11:58:11 +0300

    fix: remove useless output

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -2,6 +2,6 @@ output "endpoint" {
   value = aws_eks_cluster.cluster.endpoint
 }
 
-output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.cluster.certificate_authority[0].data
+output "cluster_name" {
+  value = aws_eks_cluster.cluster.name
 }
